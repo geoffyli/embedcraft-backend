0da4c62406dc7ec43ae779614232dea58eeba951
geoffyli <geoff.yulong.li@gmail.com>
2024-02-26 21:36:44 -0500

    Create Spring Boot template for embedcraft-core.

---
+++ embedcraft-core/src/main/java/com/embedcraft/embedcraftcore/EmbedcraftCoreApplication.java
@@ -0,0 +1,13 @@
+package com.embedcraft.embedcraftcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbedcraftCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbedcraftCoreApplication.class, args);
+	}
+
+}
