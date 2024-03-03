46c66a5037e6f8dacd33c9ac5b6d299b1b819eb2
geoffyli <geoff.yulong.li@gmail.com>
2024-03-03 17:07:04 -0500

    Configure Mybatis 3.0.3 for the project.

--- embedcraft-core/src/main/java/com/embedcraft/embedcraftcore/EmbedcraftCoreApplication.java
+++ embedcraft-core/src/main/java/com/embedcraft/embedcraftcore/EmbedcraftCoreApplication.java
@@ -1,9 +1,11 @@
 package com.embedcraft.embedcraftcore;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.embedcraft.embedcraftcore.mapper") // Mapper package path
 public class EmbedcraftCoreApplication {
 
 	public static void main(String[] args) {
