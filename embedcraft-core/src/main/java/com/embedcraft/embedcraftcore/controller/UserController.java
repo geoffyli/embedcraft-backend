ad15dff59142cbd37b37087fde12218f2b8c406c
geoffyli <geoff.yulong.li@gmail.com>
2024-03-05 21:32:25 -0500

    Complete user login controller method.

--- embedcraft-core/src/main/java/com/embedcraft/embedcraftcore/controller/UserController.java
+++ embedcraft-core/src/main/java/com/embedcraft/embedcraftcore/controller/UserController.java
@@ -2,10 +2,15 @@ package com.embedcraft.embedcraftcore.controller;
 
 import com.embedcraft.embedcraftcore.VO.UserInfoVO;
 import com.embedcraft.embedcraftcore.service.UserService;
+import com.embedcraft.embedcraftcore.util.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,13 +21,24 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/login")
-    public Map<String, Object> login (@RequestBody UserInfoVO userInfo){
-        Integer userID = userService.login(userInfo.getAccount(), userInfo.getPassword());
+    public ResponseEntity<Map<String, Object>> login (@RequestBody UserInfoVO userInfo){
         Map<String, Object> responseBody = new HashMap<>();
-        responseBody.put("res", userID);
+
+        Integer userID = userService.login(userInfo.getAccount(), userInfo.getPassword());
+        // If login failed (userID < 0)
+        if (userID < 0){
+            responseBody.put("error", "Invalid login credentials.");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(responseBody);
+        }
+        responseBody.put("userId", userID);
         // TODO: JWT Authorization
-        // ...
-        return responseBody;
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("Authorization", JWTUtil.createJWT(userID));
+
+        return ResponseEntity.ok()
+                .headers(responseHeaders)
+                .body(responseBody);
 
     }
 }
